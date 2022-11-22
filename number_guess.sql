aca078967b4c560a178a3bf75fe5aad7c34060a3
Manuel Pedraza <111818883+MPedrazam@users.noreply.github.com>
2022-11-21 22:03:44 -0500

    Update number_guess.sql

--- number_guess.sql
+++ number_guess.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE number_guess;
 
-CREATE TABLE number_guess(user_name VARCHAR(20) NOT NULL,
+CREATE TABLE number_guess(user_name VARCHAR(22) NOT NULL,
                             guessing_attempts INT,
                             guess_id SERIAL);
